f0b37737e2857e2536e19012615859a266d97be4
BuildTools <magneticchen@gmail.com>
2018-07-25 21:31:06 +0800

    small changes

--- NSF/javascript/NSd/utilities.js
+++ NSF/javascript/NSd/utilities.js
@@ -119,7 +119,7 @@ function s4() {
   return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
 }
 
-addDays = function(date, days) {
+addDays = (date, days)=> {
   var dat = date;
   dat.setDate(dat.getDate() + days);
   return dat;
