2ce25cc9d9e135bcf03a4c2ee9d6b66815255c21
yves <magneticchen@gmail.com>
2018-09-29 00:25:20 +0800

    fix notalk

--- NSF/javascript/services/NoTalk/NoTalk.js
+++ NSF/javascript/services/NoTalk/NoTalk.js
@@ -1,1 +1,11 @@
-module.exports = NoUser;
+// NoTalk.js
+// Description:
+// "NoTalk.js" NOOXY Talk Service.
+// Copyright 2018 NOOXY. All Rights Reserved.
+
+
+function NoTalk() {
+  this.test= '123';
+};
+
+module.exports = NoTalk;
