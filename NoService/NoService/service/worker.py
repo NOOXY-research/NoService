136cd7461182a11a63b23ffd02502e2693614cd7
yves <magneticchen@gmail.com>
2019-01-14 00:03:16 +0800

    init python service support

--- NoService/NoService/service/worker.py
+++ NoService/NoService/service/worker.py
@@ -16,5 +16,34 @@
 from twisted.internet import reactor, protocol
 
 class NoService(self):
-    def API():
-        pass
+    pass
+
+class EchoClient(protocol.Protocol):
+    """Once connected, send a message, then print the result."""
+
+    def connectionMade(self):
+        self.transport.write(b"hello, world!")
+
+    def dataReceived(self, data):
+        "As soon as any data is received, write it back."
+        print("Server said:", data)
+        self.transport.loseConnection()
+
+    def connectionLost(self, reason):
+        print("connection lost")
+
+class EchoFactory(protocol.ClientFactory):
+    protocol = EchoClient
+
+    def clientConnectionFailed(self, connector, reason):
+        print("Connection failed - goodbye!")
+        reactor.stop()
+
+    def clientConnectionLost(self, connector, reason):
+        print("Connection lost - goodbye!")
+        reactor.stop()
+
+f = EchoFactory()
+
+reactor.connectUNIX("/tmp/noservice.sock", f)
+reactor.run()
