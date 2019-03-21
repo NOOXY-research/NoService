e675bc35bf93175f6e5f2d6488afb2c82ba34bae
BuildTools <magneticchen@gmail.com>
2019-03-21 20:01:39 +0800

    copyright

--- NoService/launch.js
+++ NoService/launch.js
@@ -1,7 +1,7 @@
 // NoService/launch.js
 // Description:
 // "launch.js" launch NOOXY Service deamon.
-// Copyright 2018 NOOXY. All Rights Reserved.
+// Copyright 2019 NOOXY. All Rights Reserved.
 
 const Launcher = require('./NoService').Runtime.Launcher;
 const Path = require("path");
