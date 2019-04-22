8c60504413394139ca939d634c368e734d8cf978
BuildTools <magneticchen@gmail.com>
2019-04-23 01:17:42 +0800

    update readme.md about resource network

---
+++ NoService/services/NoCluster/plugin/virtualization/neuralnet.js
@@ -0,0 +1,4 @@
+// NoService/NoService/rumtime/plugins/virtualization/neuralnet.js
+// Description:
+// "neuralnet.js" use cluster API to virtualize neuralnet etc.
+// Copyright 2018-2019 NOOXY. All Rights Reserved.
